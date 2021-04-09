968bcaccd1bdbab01b949d6cf21f996fd4c58428
Caroline Hatwell <caroline.hatwell@coopdigital.co.uk>
2021-04-09 15:28:09 +0100

    add new test, let's see what circle ci does

--- tests/test_getbooks.py
+++ tests/test_getbooks.py
@@ -7,6 +7,9 @@ class GetBooksTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
+    def test_for_the_sake_of_it(self):
+        self.assertEqual(9, 3*3)
+
     def test_successful_getbooks(self):
 
         response = self.app.get('/books', headers={"Content-Type": "application/json"})
